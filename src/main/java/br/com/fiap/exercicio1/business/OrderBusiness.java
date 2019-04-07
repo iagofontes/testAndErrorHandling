0b48c1fa2d96ac5019dcbfe7d4f22cacf69e5fdd
Iago <iagofontes@hotmail.com>
2019-04-07 13:05:00 -0300

    initial commit

--- src/main/java/br/com/fiap/exercicio1/business/OrderBusiness.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.exercicio1.business;
-
-public class OrderBusiness {
-}
--- src/test/java/br/com/fiap/exercicio1/repository/OrderRepositoryImplTest.java
+++ src/test/java/br/com/fiap/exercicio1/repository/OrderRepositoryImplTest.java
@@ -2,15 +2,24 @@ package br.com.fiap.exercicio1.repository;
 
 import br.com.fiap.exercicio1.dto.OrderDTO;
 import br.com.fiap.exercicio1.rest.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(OrderDTO.class)
 public class OrderRepositoryImplTest {
 
     @Autowired
@@ -26,17 +35,67 @@ public class OrderRepositoryImplTest {
         orderDTO.setIdPedido(1);
         when(this.repository.findById(orderDTO.getIdPedido())).thenReturn(orderDTO);
 
+        verify(orderDTO).setIdPedido(1);
+    }
+
+    @Test
+    public void findById2() throws Exception {
+        OrderDTO orderDTO = mock(OrderDTO.class);
+
+        orderDTO.setIdPedido(1);
+        when(this.repository.findById(orderDTO.getIdPedido())).thenReturn(orderDTO);
+
+        mvc.perform(get("/findbyid/"+orderDTO.getIdPedido())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(response -> {
+                    String json = response.getResponse().getContentAsString();
+                    OrderDTO orderFounded = new ObjectMapper().readValue(json, OrderDTO.class);
+                    assertNotNull("Order null", orderFounded);
+                });
+
+
+        verify(orderDTO).setIdPedido(1);
     }
 
     @Test
     public void save() {
+        OrderDTO orderDTO = mock(OrderDTO.class);
+
+        orderDTO.setIdPedido(1);
+        orderDTO.setNomeCompleto("João Silva");
+        orderDTO.setBandeira("Visa");
+        orderDTO.setFormaPagto("CREDITO");
+        when(this.repository.save(orderDTO)).thenReturn(orderDTO);
+
+        verify(orderDTO).setIdPedido(1);
+        verify(orderDTO).setNomeCompleto("João Silva");
+        verify(orderDTO).setBandeira("Visa");
+        verify(orderDTO).setFormaPagto("CREDITO");
     }
 
     @Test
     public void update() {
+        OrderDTO orderDTO = mock(OrderDTO.class);
+
+        orderDTO.setIdPedido(1);
+        orderDTO.setNomeCompleto("João Silva");
+        orderDTO.setBandeira("MASTER");
+        orderDTO.setFormaPagto("DEBITO");
+        when(this.repository.update(orderDTO)).thenReturn(orderDTO);
+
+        verify(orderDTO).setIdPedido(1);
+        verify(orderDTO).setNomeCompleto("João Silva");
+        verify(orderDTO).setBandeira("MASTER");
+        verify(orderDTO).setFormaPagto("DEBITO");
     }
 
     @Test
     public void delete() {
+        OrderDTO orderDTO = mock(OrderDTO.class);
+
+        orderDTO.setIdPedido(1);
+        assertEquals("Excluído com sucesso.", this.repository.delete(orderDTO.getIdPedido()));
+
     }
 }
